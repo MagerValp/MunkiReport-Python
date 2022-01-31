f7586af4eca6731914d851341a3a8aafdd131c07
Per Olofsson <magervalp@fastmail.fm>
2022-01-31 14:32:36 +0100

    Import project

---
+++ build-munkireport-python.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+
+set -o errexit
+
+
+if ! command -v munkipkg &> /dev/null; then
+	echo "This script requires munkipkg, https://github.com/munki/munki-pkg"
+	exit 69
+fi
+
+
+rm -rf payload/*
+
+FWROOT="payload/Library/MunkiReport"
+mkdir -p "$FWROOT"
+
+# Build a relocatable Python framework
+echo "🔶 Creating relocatable Python 2.7.18 framework"
+echo "Logging output to relocatable-python.log"
+(
+	cd relocatable-python
+	./make_relocatable_python_framework.py \
+		--upgrade-pip \
+		--pip-requirements=../requirements.txt \
+		--python-version 2.7.18 \
+		--destination "../$FWROOT" \
+		> ../relocatable-python.log 2>&1
+)
+fwpath="$FWROOT/Python.framework"
+verpath="$fwpath/Versions/2.7"
+sppath="$verpath/lib/python2.7/site-packages"
+
+# Stub out framework import check for macOS 11+
+echo "🔸 Patching PyObjC"
+sed -i '' '85s/os.path.exists(f)/True/' "$sppath/objc/_dyld.py"
+"$verpath/bin/python" -m compileall "$sppath/objc/_dyld.py"
+
+# Create the symlink
+echo "🔹 Creating symlink"
+mkdir -p payload/usr/local/munkireport
+ln -s /Library/MunkiReport/Python.framework/Versions/2.7/bin/python payload/usr/local/munkireport/munkireport-python
+
+
+# Build the package
+echo "📦 Building package"
+munkipkg .
+
+
+echo "✅ Done"
+
+exit 0
