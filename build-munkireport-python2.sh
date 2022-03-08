67292122b4585585279709fe510028bd8a0c4b75
Per Olofsson <magervalp@fastmail.fm>
2022-03-08 11:59:17 +0100

    Don't hardcode python version

--- build-munkireport-python2.sh
+++ build-munkireport-python2.sh
@@ -5,6 +5,8 @@ set -o errexit
 
 
 PKGDIR="Python2"
+version=$( grep '"version":' "$PKGDIR/build-info.json" | cut -d\" -f4 )
+ver_short=$( echo "$version" | cut -d. -f1,2 )
 
 rm -rf "$PKGDIR/payload"/*
 
@@ -12,20 +14,20 @@ FWROOT="$PKGDIR/payload/Library/MunkiReport"
 mkdir -p "$FWROOT"
 
 # Build a relocatable Python framework
-echo "🔶 Creating relocatable Python 2.7.18 framework"
+echo "🔶 Creating relocatable Python $version framework"
 echo "Logging output to relocatable-python.log"
 (
 	cd relocatable-python
 	./make_relocatable_python_framework.py \
 		--upgrade-pip \
 		--pip-requirements=../$PKGDIR/requirements.txt \
-		--python-version 2.7.18 \
+		--python-version "$version" \
 		--destination "../$FWROOT" \
 		> ../relocatable-python.log 2>&1
 )
 fwpath="$FWROOT/Python.framework"
-verpath="$fwpath/Versions/2.7"
-sppath="$verpath/lib/python2.7/site-packages"
+verpath="$fwpath/Versions/$ver_short"
+sppath="$verpath/lib/python$ver_short/site-packages"
 
 # Stub out framework import check for macOS 11+
 echo "🔸 Patching PyObjC"
@@ -35,7 +37,7 @@ sed -i '' '85s/os.path.exists(f)/True/' "$sppath/objc/_dyld.py"
 # Create the symlink
 echo "🔹 Creating symlink"
 mkdir -p "$PKGDIR/payload/usr/local/munkireport"
-ln -s /Library/MunkiReport/Python.framework/Versions/2.7/bin/python "$PKGDIR/payload/usr/local/munkireport/munkireport-python2"
+ln -s "/Library/MunkiReport/Python.framework/Versions/$ver_short/bin/python" "$PKGDIR/payload/usr/local/munkireport/munkireport-python2"
 
 
 # Build the package
